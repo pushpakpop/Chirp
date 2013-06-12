d308ae033bbb88b355040a641639c38934be13a0
Pushpak Patel <pushpak.pop@gmail.com>
2013-06-12 19:57:13 +0530

    index page updated
    1. Added chirp logo and login with twitter button on index page
    
    2. tweets.pdf deleted.

--- index.php
+++ index.php
@@ -11,11 +11,31 @@ header('Location: login.php ');
 <!DOCTYPE html>
 <html>
 <head>
+<style type="text/css">
+body {
+	text-align: center;
+	background: #545454;
+	}
+#wrapper {
+	margin-top:8%;
+}
+h1 {
+	font-size: 40px;
+	color: #FFF	;
+	margin-top: 0px;
+	margin-bottom: 40px;
+}
+#twitter {
+	width: 200px;
+	}
+</style>
 </head>
 
 <body>
-	<div>
-		<a href="login.php">Login with twitter</a>
+	<div id="wrapper">
+    	<img src="images/Chirp.png">
+        <h1>Chirp</h1>
+		<p><a href="login.php"><img id="twitter" src="images/login-twitter.png" alt=""></a></p>.
 	</div>
 </body>
 </html>
