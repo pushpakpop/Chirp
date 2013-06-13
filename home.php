67b1429ca679fd9b8cc5cef422f0e2aef9086cec
Pushpak Patel <pushpak.pop@gmail.com>
2013-06-13 16:31:42 +0530

    place jquery under js folder

--- home.php
+++ home.php
@@ -150,7 +150,7 @@ $friend_list = $twitteroauth->get("https://api.twitter.com/1.1/followers/list.js
 </div><!--//.notification-->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="lib/bootstrap/js/jquery-1.8.1.min.js"><\/script>')</script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.8.1.min.js"><\/script>')</script>
 <script type="text/javascript" src="lib/handlebars.js"></script>
 <script type="text/javascript" src="lib/moment.js"></script>
 <script type="text/javascript" src="lib/bootstrap-modal.js"></script>
