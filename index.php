db5eb21684dfcc384259c325169ecf3f65a569b1
Pushpak Patel <pushpak.pop@gmail.com>
2013-06-14 14:17:25 +0530

    redirect location changed to timeline#home

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 // Check if user has already logged in to the application
 if (isset($_SESSION['access_token']) || isset($_SESSION['access_token']['oauth_token']) || isset($_SESSION['access_token']['oauth_token_secret']))
 {
-    header('Location: home');
+    header('Location: timeline#home');
 }
 ?>
 <!DOCTYPE html>
